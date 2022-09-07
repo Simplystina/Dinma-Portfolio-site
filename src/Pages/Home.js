16e701386495f2109eafc3cc997e4d075bc0fa43
Simplystina <62130713+Simplystina@users.noreply.github.com>
2022-09-07 23:08:10 +0100

    updated the logo

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 import React,{useEffect} from 'react'
-import {Box, Flex, HStack, Text} from "@chakra-ui/react"
+import {Box, Flex, HStack, Text, Img} from "@chakra-ui/react"
 import { NavLink, Outlet } from 'react-router-dom'
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -7,6 +7,7 @@ import {AiFillHome} from "react-icons/ai"
 import {BsFillFilePersonFill} from "react-icons/bs"
 import {FaLaptop} from "react-icons/fa"
 import {BiMessageDots} from "react-icons/bi"
+import logo from "../images/Dinma-logo.png"
 
 const Home = () => {
 
@@ -19,7 +20,8 @@ const Home = () => {
   return (
     <Box pos="relative" bg="black" h="100vh" overflowX="hidden" overflowY="scroll" >
         <Flex p={["30px 50px", "30px 30px", "30px 80px"]} justifyContent="space-between" borderBottom="1px solid grey" borderTop="2px solid #10c6bb">
-            <Text data-aos="zoom-in-up" data-aos-duration="3000" fontSize={[15,20]} className="name_icon">Dinma</Text>
+            <Img src={logo} alt="logo"></Img>
+           {/* <Text data-aos="zoom-in-up" data-aos-duration="3000" fontSize={[15,20]} className="name_icon">Dinma</Text> */}
             <HStack className='nav_link_container' display={["none", "Flex"]} data-aos="fade-left" color="#10c6bb" fontSize={[14, 16, 20]} fontWeight="500" spacing={[10,10, 10, 20]}>
                 <NavLink className="nav_link" to="/" style={({ isActive }) => ({ borderBottom: isActive? "1px solid white" :"none", color: isActive? "white": "#10c6bb"})}>
                     <Text>Home</Text>
